9d06ec6ea793abad6da7573d5bbd673ec32748ff
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-12-20 16:45:26 -0800

    simple search on multiple fields

--- app/controllers/backend_simple_search_controller.rb
+++ app/controllers/backend_simple_search_controller.rb
@@ -2,7 +2,10 @@ class BackendSimpleSearchController < ApplicationController
   def index
     @results = CSV.read('db/netflix_titles.csv', headers: true)
 
-    @query_string = params[:q]
-    @results = @results.select { |r| r['title'].downcase.include?(@query_string.downcase) }
+    @query_string = params[:q].downcase
+    @results = @results.select do |r|
+      r['title'].downcase.include?(@query_string) ||
+        r['cast']&.downcase&.include?(@query_string)
+    end
   end
 end
