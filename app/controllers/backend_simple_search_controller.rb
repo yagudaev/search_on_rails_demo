a77455d0363e6a4446af289998b53f4a8609c989
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-12-20 16:41:48 -0800

    get simple title search working

--- app/controllers/backend_simple_search_controller.rb
+++ app/controllers/backend_simple_search_controller.rb
@@ -1,5 +1,8 @@
 class BackendSimpleSearchController < ApplicationController
   def index
-    @results = CSV.read('db/netflix_titles.csv')
+    @results = CSV.read('db/netflix_titles.csv', headers: true)
+
+    @query_string = params[:q]
+    @results = @results.select { |r| r['title'].include?(@query_string) }
   end
 end
