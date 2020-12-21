fbd315e2bed18508eb256b9cd3b5dc2ea9be947f
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-12-20 18:14:34 -0800

    add highligthing

--- app/controllers/backend_simple_search_controller.rb
+++ app/controllers/backend_simple_search_controller.rb
@@ -7,18 +7,51 @@ class BackendSimpleSearchController < ApplicationController
       match_by_word(r['title'].downcase, @query_string) ||
         match_by_word(r['cast']&.downcase, @query_string)
     end
+
+    @results = @results.select do |r|
+      match_by_word(r['title'].downcase, @query_string) ||
+        match_by_word(r['cast']&.downcase, @query_string)
+    end
+
+    @results = add_highlights(@results)
   end
 
   private
 
   def match_by_word(test_string, query_string)
-    return true unless query_string
-    return false unless test_string
+    return true unless query_string.present?
+    return false unless test_string.present?
 
     query_string.split(' ').any? { |word| test_string.include?(word) }
   end
 
   def remove_stop_words(str)
-    str.gsub(/\bthe|a|an|of\b/, '')
+    str.gsub(/\b(the|a|an|of|to)\b/, '')
+  end
+
+  def add_highlights(results)
+    results.each do |result|
+      result['title'] = add_highlight(result['title'].downcase)
+      result['cast'] = add_highlight(result['cast'].downcase) if result['cast']
+    end
+  end
+
+  def add_highlight(str)
+    words = @query_string.split(' ')
+
+    words.reduce(str) do |accumulator, word|
+      start_pos = accumulator.index(word)
+
+      next str unless start_pos
+
+      end_pos = start_pos + word.length
+
+      opening_tag = '<b>'
+      closing_tag = '</b>'
+      new_str = accumulator.insert(start_pos, opening_tag)
+      new_str = new_str.insert(end_pos + opening_tag.length, closing_tag)
+
+      new_str
+    end
   end
 end
