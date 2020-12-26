dd5355ace06a7e140b00936c6196c5ef16983455
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-12-26 11:13:57 -0800

    add tests for the rest of the simple search

--- spec/lib/search/in_memory_spec.rb
+++ spec/lib/search/in_memory_spec.rb
@@ -30,13 +30,22 @@ RSpec.describe Search::InMemory do
     end
 
     context 'when the collection has many items' do
+      let(:record_1) { { title: 'Die Hard', description: 'Great movie' } }
+      let(:record_2) { { title: 'Titanic', description: 'Sad movie' } }
+      let(:collection) { [record_1, record_2] }
+
       context 'and searching using an emtpy string' do
+        it { is_expected.to match_array(collection) }
       end
 
       context 'and there is a match' do
+        let(:query) { 'Die' }
+        it { is_expected.to match_array([record_1]) }
       end
 
       context 'and there is no match' do
+        let(:query) { 'Alien' }
+        it { is_expected.to match_array([]) }
       end
     end
   end
