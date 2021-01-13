6fbcd78e757fc1540dbdf7862d67bf73afcc9fc6
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-13 09:54:29 -0800

    comment out the combined spec

--- spec/lib/search/in_memory_spec.rb
+++ spec/lib/search/in_memory_spec.rb
@@ -4,12 +4,13 @@ require 'lib/search/in_memory/shared_examples/matching'
 RSpec.describe Search::InMemory do
   it_behaves_like 'matcher'
 
-  describe '.search' do
-    subject { instance.search(collection, query) }
+  # combines all
+  # describe '.search' do
+  #   subject { instance.search(collection, query) }
 
-    let(:collection) { [] }
-    let(:query) { '' }
-    let(:instance) { described_class.new }
+  #   let(:collection) { [] }
+  #   let(:query) { '' }
+  #   let(:instance) { described_class.new }
 
-  end
+  # end
 end
