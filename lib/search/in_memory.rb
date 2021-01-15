2256c6e0c08c466dbd90754e6aa1117b9f2d800b
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-15 09:44:03 -0800

    remove all tests and implementation

--- lib/search/in_memory.rb
+++ lib/search/in_memory.rb
@@ -5,18 +5,7 @@ module Search
     end
 
     def match(records, query_string)
-      records.select do |r|
-        r.keys.any? { |key| match_by_word(r[key], query_string) }
-      end
-    end
-
-    private
-
-    def match_by_word(test_string, query_string)
-      return true if query_string.blank?
-      return false if test_string.blank?
-
-      query_string.split.any? { |word| test_string.include?(word) }
+      records
     end
   end
 end
