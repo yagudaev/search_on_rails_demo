c2938c5e6a661a43fe163c6f9d94cbb663eb5001
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-22 10:13:38 -0800

    install tailwind using rails bytes

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+    plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+
+}
