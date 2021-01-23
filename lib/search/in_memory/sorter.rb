3a5171709de8a381eccd55ee3b66fb79763600d4
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-23 14:33:49 -0800

    get sorting working

--- lib/search/in_memory/sorter.rb
+++ lib/search/in_memory/sorter.rb
@@ -5,7 +5,12 @@ module Search
         return collection if collection.length <= 1
         return collection unless sort_options
 
-        collection.sort_by { |record| record[sort_options[:field]&.to_sym] }
+        collection.sort do |record_a, record_b|
+          a = record_a[sort_options[:field]&.to_sym]
+          b = record_b[sort_options[:field]&.to_sym]
+
+          sort_options[:direction] == 'ascending' ? a <=> b : b <=> a
+        end
       end
     end
   end
