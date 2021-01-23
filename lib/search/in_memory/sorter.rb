80a0d79e4e1040dae72e1e457832dde7d3412e17
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-23 12:42:23 -0800

    got most sorting working

--- lib/search/in_memory/sorter.rb
+++ lib/search/in_memory/sorter.rb
@@ -1,8 +1,11 @@
 module Search
   class InMemory
     module Sorter
-      def sort(collection, _query, _sort_options = {})
-        collection
+      def sort(collection, _query, sort_options = nil)
+        return collection if collection.length <= 1
+        return collection unless sort_options
+
+        collection.sort_by { |record| record[sort_options[:field]&.to_sym] }
       end
     end
   end
--- spec/lib/search/in_memory/shared_examples/sorter.rb
+++ spec/lib/search/in_memory/shared_examples/sorter.rb
@@ -1,14 +1,40 @@
 RSpec.shared_examples Search::InMemory::Sorter do
-  subject { instance.sort(collection, query) }
+  subject { instance.sort(collection, query, sort) }
 
   let(:collection) { [] }
   let(:query) { '' }
   let(:instance) { described_class.new }
+  let(:sort) { nil }
 
   context 'when given an empty collection' do
     it { is_expected.to match_array([]) }
   end
 
-  context 'when the collection has one or more items' do
+  context 'when the collection has one item' do
+    let(:collection) { [record] }
+    let(:record) { { title: 'A' } }
+
+    it { is_expected.to eq([record]) }
+  end
+
+  context 'when the collection has many items' do
+    let(:collection) { [record_1, record_2, record_3] }
+    let(:record_1) { { title: 'B' } }
+    let(:record_2) { { title: 'A' } }
+    let(:record_3) { { title: 'C' } }
+
+    it { is_expected.to eq([record_1, record_2, record_3]) }
+
+    describe 'and sorting by ascending order' do
+      let(:sort) { { field: 'title', direction: 'ascending' } }
+
+      it { is_expected.to eq([record_2, record_1, record_3]) }
+    end
+
+    describe 'and sorting by descending order' do
+      let(:sort) { { field: 'title', direction: 'descending' } }
+
+      it { is_expected.to eq([record_3, record_1, record_2]) }
+    end
   end
 end
