f8643fe8f9c9d91057a4675c090519616e1fe22d
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-22 19:33:43 -0800

    add pagination

--- app/controllers/in_memory_search_controller.rb
+++ app/controllers/in_memory_search_controller.rb
@@ -2,5 +2,7 @@ class InMemorySearchController < ApplicationController
   def index
     @results = Title.import
     @results = Title.search(params[:q]) if params[:q]
+
+    @pagy, @results_page = pagy_array(@results)
   end
 end
