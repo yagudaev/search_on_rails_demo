aa517cd57404c32cc426d8f501da6c4e99543379
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-23 22:49:40 -0800

    hook up select controller

--- app/controllers/in_memory_search_controller.rb
+++ app/controllers/in_memory_search_controller.rb
@@ -4,6 +4,7 @@ class InMemorySearchController < ApplicationController
 
     @results = Title.search(params[:q] || '', sort: params[:sort])
     @sort_by = params.dig(:sort, :field) == 'title' ? "title_#{params.dig(:sort, :direction)}" : 'other'
+    @sort_options = [['Relevance', '_score_desc'], ['Title A-Z', 'title_asc'], ['Title Z-A', 'title_desc'], ['Other', 'other']]
 
     @pagy, @results_page = pagy_array(@results)
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,20 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application.scss"
 
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.handleSortChange = (event) => {
+  const target = event.target
+  const sort = target.value
+
+  if (['_score_desc', 'other'].includes(sort)) return
+
+  const match = sort.match(/(.+)_(desc|asc)$/)
+  const field = match[1]
+  const direction = match[2]
+
+  const location = `sort[field]=${field}&sort[direction]=${direction}`
+  window.location.search = location
+}
