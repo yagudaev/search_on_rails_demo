42e4500a5c42319dd80cee4030540beb97ca9a39
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-23 21:48:45 -0800

    get the select to work

--- app/controllers/in_memory_search_controller.rb
+++ app/controllers/in_memory_search_controller.rb
@@ -3,6 +3,7 @@ class InMemorySearchController < ApplicationController
     @results = Title.import
 
     @results = Title.search(params[:q] || '', sort: params[:sort])
+    @sort_by = params.dig(:sort, :field) == 'title' ? "title_#{params.dig(:sort, :direction)}" : 'other'
 
     @pagy, @results_page = pagy_array(@results)
   end
