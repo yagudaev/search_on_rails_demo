0bfe3a911f222555a4696833764552c343bcbbc6
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-01-25 21:38:13 -0800

    add basic test for scoring and implementation

--- lib/search/in_memory/scorer.rb
+++ lib/search/in_memory/scorer.rb
@@ -1,10 +1,27 @@
 module Search
   class InMemory
     module Scorer
-      def score(collection, _query, _weights = nil)
+      BASE_POWER = 10
+
+      def score(collection, query, _weights = nil)
         collection.map do |record|
-          { **record, _score: 1 }
+          total_score = record.reduce(0) do |total, (key, value)|
+            next total unless value.is_a?(String)
+
+            total + field_score(value, query)
+          end
+          { **record, _score: total_score }
         end
+      end
+
+      def field_score(value, query)
+        return 1 if query == value
+
+        pos = value.index(query)
+        return 0 unless pos
+        return 0.7 if pos == 0
+
+        0.4
       end
     end
   end
--- spec/lib/search/in_memory/shared_examples/scorer.rb
+++ spec/lib/search/in_memory/shared_examples/scorer.rb
@@ -10,10 +10,41 @@ RSpec.shared_examples Search::InMemory::Scorer do
     it { is_expected.to match_array([]) }
   end
 
-  context 'when the collection has one item' do
+  context 'when the collection has one item and a match' do
     let(:collection) { [record] }
-    let(:record) { { title: 'A', _score: 1 } }
+    let(:record) { { title: 'Matrix' } }
+    let(:query) { 'Matrix' }
 
-    it { is_expected.to eq([record]) }
+    describe 'full match' do
+      it { is_expected.to eq([{ **record, _score: 1 }]) }
+    end
+
+    describe 'partial match start' do
+      let(:record) { { title: 'Matrix: Reloaded' } }
+
+      it { is_expected.to eq([{ **record, _score: 0.7 }]) }
+    end
+
+    describe 'partial match middle or end' do
+      let(:record) { { title: 'The Matrix: Reloaded' } }
+
+      it { is_expected.to eq([{ **record, _score: 0.4 }]) }
+    end
+
+    describe 'no match' do
+      let(:record) { { title: 'Something Else' } }
+
+      it { is_expected.to eq([{ **record, _score: 0 }]) }
+    end
   end
+
+  # weights for fields power
+  # full-match ratio 1  10
+  # start partial-match 0.7 * 10^(weight)
+  # middle/end partial match 0.4 * 10^(weight)
+  # multiple matches
+
+  # position based pass in the match locations
+
+  # fuzzy-match ?
 end
