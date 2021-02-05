2fd50e563655c992efc84a98a22ae072cc1471f2
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-02-04 21:43:33 -0800

    update matcher

--- lib/search/in_memory/matcher.rb
+++ lib/search/in_memory/matcher.rb
@@ -1,20 +1,12 @@
 module Search
   class InMemory
     module Matcher
-      def match(records, query_string, searchable_fields = nil)
-        records.select do |r|
-          keys = searchable_fields || r.keys
-          keys.any? { |key| match_by_word(r[key], query_string) }
+      def match(records, query_string)
+        return records if query_string.blank?
+
+        records.select do |record|
+          record.values.any? { |value| value&.match(query_string) }
         end
-      end
-
-      private
-
-      def match_by_word(test_string, query_string)
-        return true if query_string.blank?
-        return false if test_string.blank?
-
-        query_string.split.any? { |word| test_string.include?(word) }
       end
     end
   end
