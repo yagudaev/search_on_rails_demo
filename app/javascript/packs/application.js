9f6cc81b49c6449b99d4d0abbc551773a0521782
Michael Yagudaev <michael@nano3labs.com>
2021-04-16 13:29:08 -0700

    Improve filter submission to use Turbolinks (#10)
    * Add ransack gem
    
    * Improve form submission to use turbolinks
    
    * Add source
    
    * Add prettier config
    
    * Update how sorting works
    
    * change to append instead of set

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,14 +17,41 @@ window.handleSortChange = (event) => {
   const target = event.target
   const sort = target.value
 
-  if (['other'].includes(sort)) return
+  if (["other"].includes(sort)) return
 
   const match = sort.match(/(.+)_(desc|asc)$/)
   const field = match[1]
   const direction = match[2]
   const params = new URLSearchParams(window.location.search)
-  const query = params.get('q')
+  params.set('sort[field]', field)
+  params.set('sort[direction]', direction)
 
-  const location = `q=${query}&sort[field]=${field}&sort[direction]=${direction}`
-  window.location.search = location
+  const location = `${window.location.pathname}?${params.toString()}`
+  Turbolinks.visit(location)
 }
+
+window.handleFormSubmit = (target) => {
+  target.form.requestSubmit()
+}
+
+function makeGetFormsSubmitWithTurbolinks() {
+  // Source: https://github.com/turbolinks/turbolinks/issues/272#issuecomment-615038967
+  document.addEventListener("turbolinks:load", function (event) {
+    for (let form of document.querySelectorAll("form[method=get]:not([data-remote=true])")) {
+      form.addEventListener("submit", function (event) {
+        event.preventDefault()
+        const entries = [...new FormData(event.target).entries()]
+        const actionUrl = new URL(event.target.action)
+        const currentUrl = new URL(location.href)
+        // if pathname not changed, hand over per parameter to next page.
+        if (actionUrl.pathname === currentUrl.pathname && currentUrl.searchParams.has("per")) {
+          actionUrl.searchParams.set("per", currentUrl.searchParams.get("per"))
+        }
+        entries.forEach((entry) => actionUrl.searchParams.append(...entry))
+        Turbolinks.visit(actionUrl.toString())
+      })
+    }
+  })
+}
+
+makeGetFormsSubmitWithTurbolinks()
