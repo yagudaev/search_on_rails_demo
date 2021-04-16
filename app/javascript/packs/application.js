f393131a97b840f00e7524cabaa4b4946dc600d5
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-04-16 12:48:42 -0700

    Add source

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,12 +23,8 @@ window.handleSortChange = (event) => {
   const field = match[1]
   const direction = match[2]
   const params = new URLSearchParams(window.location.search)
-<<<<<<< HEAD
   params.set('sort[field]', field)
   params.set('sort[direction]', direction)
-=======
-  const query = params.get("q")
->>>>>>> Improve form submission to use turbolinks
 
   const location = `${window.location.pathname}?${params.toString()}`
   Turbolinks.visit(location)
@@ -39,11 +35,7 @@ window.handleFormSubmit = (target) => {
 }
 
 function makeGetFormsSubmitWithTurbolinks() {
-<<<<<<< HEAD
   // Source: https://github.com/turbolinks/turbolinks/issues/272#issuecomment-615038967
-=======
-  // Source:
->>>>>>> Improve form submission to use turbolinks
   document.addEventListener("turbolinks:load", function (event) {
     for (let form of document.querySelectorAll("form[method=get]:not([data-remote=true])")) {
       form.addEventListener("submit", function (event) {
@@ -52,20 +44,10 @@ function makeGetFormsSubmitWithTurbolinks() {
         const actionUrl = new URL(event.target.action)
         const currentUrl = new URL(location.href)
         // if pathname not changed, hand over per parameter to next page.
-<<<<<<< HEAD
         if (actionUrl.pathname === currentUrl.pathname && currentUrl.searchParams.has("per")) {
           actionUrl.searchParams.set("per", currentUrl.searchParams.get("per"))
         }
         entries.forEach((entry) => actionUrl.searchParams.append(...entry))
-=======
-        if (
-          actionUrl.pathname === currentUrl.pathname &&
-          currentUrl.searchParams.has("per")
-        ) {
-          actionUrl.searchParams.set("per", currentUrl.searchParams.get("per"))
-        }
-        entries.forEach((entry) => actionUrl.searchParams.set(...entry))
->>>>>>> Improve form submission to use turbolinks
         Turbolinks.visit(actionUrl.toString())
       })
     }
