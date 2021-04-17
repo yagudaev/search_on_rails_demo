4d4a415eff7b4976b5d21fa0807c4c45193da947
Michael Yagudaev <michaelyagudaev@gmail.com>
2021-04-17 15:53:27 -0700

    Fix form submission for safari

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "form-request-submit-polyfill"
 import "stylesheets/application.scss"
 
 Rails.start()
